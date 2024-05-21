4be914e117d8fbec8435c63fc3a17e6e058b23a9
SS-okamura <ss.okamura0509@gmail.com>
2024-05-21 17:19:23 +0900

    setting cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -2,7 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
+  const cors = require('cors');
   const app = await NestFactory.create(AppModule);
+  app.use(
+    cors({
+      origin: 'http://localhost:3001',
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
