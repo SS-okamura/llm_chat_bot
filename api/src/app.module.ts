c9b5633fb8089369a44a74900f8b3d6c0313c173
SS-okamura <ss.okamura0509@gmail.com>
2024-05-21 17:08:17 +0900

    Memo Search

--- api/src/app.module.ts
+++ api/src/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { MemoController } from './memo/memo.controller';
+import { PrismaService } from './prisma/prisma.service';
+import { MemoService } from './memo/memo.service';
+import { ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, MemoController],
+  providers: [AppService, PrismaService, MemoService, ConfigService],
 })
 export class AppModule {}
